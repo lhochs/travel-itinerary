7ced88e7693131b574c1d887bd74da3f8e37e5b3
Lily Ochs <70216903+lhochs@users.noreply.github.com>
2022-10-19 02:42:05 -0500

    solo project

--- mySoloProject/server/routes/event.routes.js
+++
@@ -1,8 +0,0 @@
-const EventController = require('../controllers/event.controller'); 
-module.exports = (app) => {
-    app.get('/api/event', EventController.findAll);
-    app.post('/api/event', EventController.createEvent); 
-    app.get('/api/event/:id', EventController.getEvent);
-    app.put('/api/event/:id', EventController.updateEvent);
-    app.delete('/api/event/:id', EventController.deleteEvent);
-};
